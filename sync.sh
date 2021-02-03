7b04d44f51a0590cc3a90f5b30dd9082703050ca
laseredlasergoat <laseredlasergoat@gmail.com>
2021-02-03 15:25:04 -0800

    updated sync again

--- sync.sh
+++ sync.sh
@@ -4,5 +4,5 @@
 git fetch #download server
 git checkout origin/main -- recipes.p #overwrite
 python3 sync.py #combine
-git commit -am "syncing recipes" recipes.p
+git commit -m "syncing recipes" recipes.p
 git push origin main
