acc9b73d04e0c53e76558ccb49154e67d156c94c
laseredlasergoat <laseredlasergoat@gmail.com>
2021-02-03 15:38:54 -0800

    added force to sync push

--- sync.sh
+++ sync.sh
@@ -5,4 +5,4 @@ git fetch #download server
 git checkout origin/main -- recipes.p #overwrite
 python3 sync.py #combine
 git commit -m "syncing recipes" recipes.p
-git push origin main
+git push -f origin main
